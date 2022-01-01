45557fd050b9cd29f0730cc33cbc8d14ae03b0f3
Anindia Adyutawati Nugraheni <nugrahenigabriella@gmail.com>
2022-01-02 00:58:53 +0700

    belum selesai sepenuhnya

---
+++ app/src/main/java/com/example/splash_bioskop_anin/Model.java
@@ -0,0 +1,40 @@
+package com.example.splash_bioskop_anin;
+
+public class Model {
+    private String nama;
+    private String teather;
+    private String nama_film;
+    private int jam;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTeather() {
+        return teather;
+    }
+
+    public void setTeather(String teather) {
+        this.teather = teather;
+    }
+
+    public String getNama_film() {
+        return nama_film;
+    }
+
+    public void setNama_film(String nama_film) {
+        this.nama_film = nama_film;
+    }
+
+    public int getJam() {
+        return jam;
+    }
+
+    public void setJam(int jam) {
+        this.jam = jam;
+    }
+}
